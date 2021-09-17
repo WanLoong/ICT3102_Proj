647be16255b4c2b24025be5e80b93a2ee87e76cf
Cerisez <1902659@sit.singaporetech.edu.sg>
2021-09-17 13:50:29 +0800

    Completed base UI with dummy records

--- app.py
+++ app.py
@@ -5,9 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
-
-
+    return render_template("index.html", beaconMacAaddress="2C:54:91:88:C9:E3", beaconLocation="SR3B", beaconTimestamp="17-09-21 12:00:17")
 
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', port=5000)
