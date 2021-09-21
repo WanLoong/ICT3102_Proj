03f83877a5379f12b5c65da8e4e285f3a40b285c
Elslin <wn-wf@hotmail.com>
2021-09-21 16:11:53 +0800

    Updated with GET request for beacon info

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request, abort, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/post', methods=["POST"])
 def testpost():
      input_json = request.get_json(force=True)
@@ -11,9 +12,24 @@ def testpost():
      return jsonify(dictToReturn)
 
 
+@app.route('/extractbeacon', methods=["GET"])
+def extract_beacon():
+    if request.method == "GET":
+        staff_id = request.args.get("staff_id")
+        start_time = request.args.get("start_time")
+        end_time = request.args.get("end_time")
+
+        return jsonify({
+            "staff_id": staff_id,
+            "start_time": start_time,
+            "end_time": end_time
+        })
+
+
 @app.route('/')
 def home():
-    return render_template("index.html", beaconMacAaddress="2C:54:91:88:C9:E3", beaconLocation="SR3B", beaconTimestamp="17-09-21 12:00:17")
+    return render_template("index.html", beaconMacAaddress="2C:54:91:88:C9:E3", beaconLocation="SR3C", beaconTimestamp="17-09-21 12:00:17")
+
 
 if __name__ == '__main__':
 
