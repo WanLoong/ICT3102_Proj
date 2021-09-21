67fea373f6b99d2476177e06779c1a7ecdea99a9
Wan Loong <55482208+lucasEun@users.noreply.github.com>
2021-09-21 10:52:38 +0800

    Added post api call to test data coming from android

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask, render_template
+
+from flask import Flask, render_template, request, abort, jsonify
 
 app = Flask(__name__)
+
+@app.route('/post', methods=["POST"])
+def testpost():
+     input_json = request.get_json(force=True)
+     dictToReturn = {'MAC_ADD':input_json['MAC_ADD'], 'Staff_ID': input_json['Staff_ID']}
+     #once data is recieved call the api
+     return jsonify(dictToReturn)
 
 
 @app.route('/')
@@ -8,4 +16,5 @@ def home():
     return render_template("index.html", beaconMacAaddress="2C:54:91:88:C9:E3", beaconLocation="SR3B", beaconTimestamp="17-09-21 12:00:17")
 
 if __name__ == '__main__':
+
     app.run(host='0.0.0.0', port=5000)
